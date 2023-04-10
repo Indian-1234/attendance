86a8cdf29a89a841448b16b957fdfe3d93a3d20a
indian-123 <e363469@gmail.com>
2023-04-10 20:33:02 +0530

    first commit

---
+++ attendance/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from attendance import models
+
+# Register your models here.
+admin.site.register(models.college)
+admin.site.register(models.Staff)
+admin.site.register(models.Student)
+admin.site.register(models.Attendance)
+admin.site.register(models.Register)
+
+
